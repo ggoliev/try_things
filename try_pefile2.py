77a0322d424070e69d54dc755c68b5d413eabe8c
Georgy Goliev <ggoliev@zscaler.com>
2022-10-02 08:52:35 +0300

    Three ways to work with conditions in list.
    LBYL, EAFP and fix for dictionary comprehension.

--- try_pefile2.py
+++ try_pefile2.py
@@ -3,6 +3,8 @@
 import pefile  # type: ignore
 from datetime import datetime
 import logging
+from more_itertools import first
+from contextlib import suppress
 
 
 def get_portable_executable_info(file_path: str) -> dict:
@@ -25,22 +27,39 @@ def get_portable_executable_info(file_path: str) -> dict:
         return decoded_dict
 
     file_info: list = pe.FileInfo  # In my case: List of lists. len==1.
-    # The nested list contains 2 elements of class 'pefile.Structure': VarFileInfo and StringFileInfo
+    # The nested list contains 2 elements of class 'pefile.Structure': [VarFileInfo] and [StringFileInfo]
+    # The [StringFileInfo] has attr StringTable that return list len==1. Contains 1 element of class 'pefile.Structure':
+    # [<Structure: [StringTable] 0x40A7A4 0x0 Length: 0x35E 0x40A7A6 0x2 ValueLength: 0x0 0x40A7A8 0x4 Type: 0x1>]
 
-    for structure in file_info[0]:
-        if not hasattr(structure, 'StringTable'):
-            logging.debug("This entry doesn't contain attribute StringTable.")
-            continue
+    # Option 1. LBYL
+    # for structure in file_info[0]:
+    #     if not hasattr(structure, 'StringTable'):
+    #         continue
+    #     list_with_string_table_structure: list = structure.StringTable
+    #     encoded_dict: dict = list_with_string_table_structure[0].entries
+    #     decoded_dict = {key.decode(): value.decode() for (key, value) in encoded_dict.items()}
+    #     break
 
-        string_table: list = structure.StringTable  # In my case:len==1. Contains 1 element of class 'pefile.Structure'
-        logging.debug("This entry CONTAINS attribute StringTable.")
-        encoded_dict: dict = string_table[0].entries
-        decoded_dict = dict((key.decode(), value.decode()) for key, value in encoded_dict.items())
+    # Option 2 EAFP
+    # for structure in file_info[0]:
+    #     with suppress(AttributeError):
+    #         list_with_string_table_structure: list = structure.StringTable
+    #         encoded_dict = list_with_string_table_structure[0].entries
+    #         decoded_dict = {key.decode(): value.decode() for key, value in encoded_dict.items()}
+    #         break
+
+    # Option 3
+    with suppress(ValueError):
+        string_file_info_structure = first(structure for structure in file_info[0] if hasattr(structure, 'StringTable'))
+        list_with_string_table_structure = string_file_info_structure.StringTable
+        encoded_dict = list_with_string_table_structure[0].entries
+        decoded_dict = {key.decode(): value.decode() for (key, value) in encoded_dict.items()}
 
     # Add modification time from FILE_HEADER
     time_date_stamp = pe.FILE_HEADER.TimeDateStamp
     modification_time = str(datetime.fromtimestamp(time_date_stamp))
     decoded_dict["Modification_time"] = modification_time
+
     return decoded_dict
 
 
